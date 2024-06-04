bb8853f490cf98ebec9a6650790bcdee806cf08a
tmetcalfe89 <tmetcalfe89@gmail.com>
2024-06-04 13:56:00 -0700

    save data, register sparklesdata
    fix #2
    fix #3

--- src/main/kotlin/us/timinc/mc/cobblemon/shinysparkles/ShinySparkles.kt
+++ src/main/kotlin/us/timinc/mc/cobblemon/shinysparkles/ShinySparkles.kt
@@ -2,6 +2,7 @@ package us.timinc.mc.cobblemon.shinysparkles
 
 import com.cobblemon.mod.common.api.Priority
 import com.cobblemon.mod.common.api.events.CobblemonEvents
+import com.cobblemon.mod.common.api.storage.player.PlayerDataExtensionRegistry
 import net.fabricmc.api.ModInitializer
 import net.minecraft.util.Identifier
 import us.timinc.mc.cobblemon.shinysparkles.blocks.ShinySparklesBlocks
@@ -9,6 +10,7 @@ import us.timinc.mc.cobblemon.shinysparkles.blocks.blockentities.BlockEntities
 import us.timinc.mc.cobblemon.shinysparkles.config.ConfigBuilder
 import us.timinc.mc.cobblemon.shinysparkles.config.ShinySparklesConfig
 import us.timinc.mc.cobblemon.shinysparkles.spawning.ShinySparkleSoulStealer
+import us.timinc.mc.cobblemon.shinysparkles.store.player.SparklesData
 
 object ShinySparkles : ModInitializer {
     val MOD_ID = "shinysparkles"
@@ -16,6 +18,7 @@ object ShinySparkles : ModInitializer {
     val config = ConfigBuilder.load(ShinySparklesConfig::class.java, MOD_ID)
 
     override fun onInitialize() {
+        PlayerDataExtensionRegistry.register(SparklesData.NAME, SparklesData::class.java)
         BlockEntities.register()
         ShinySparklesBlocks.register()
         CobblemonEvents.POKEMON_ENTITY_SPAWN.subscribe(Priority.LOWEST) { ShinySparkleSoulStealer.possiblyStealSoul(it) }
